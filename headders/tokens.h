04838ce5191c4251492293de5536cf0ffb5e2fc0
Tomas-Zgut <xzgutt00@stud.fit.vutbr.cz>
2025-07-06 17:36:12 +0200

    added somoe forgotten token types

--- headders/tokens.h
+++ headders/tokens.h
@@ -1,48 +1,47 @@
 #ifndef __TOKENS_H__
 #define __TOKENS_H__
+
+#include "opcodes.h"
 #include <stdint.h>
-typedef enum
-{
+typedef enum {
 	TOKEN_INSTRUCTION,
 	TOKEN_INT,
 	TOKEN_STRING,
+	TOKEN_BOOL,
+	TOKEN_NIL,
 	TOKEN_VARIABLE,
 	TOKEN_UNKNOWN,
 	TOKEN_EOF,
 } token_type;
 
-typedef enum
-{
+typedef enum {
 	GF,
 	LF,
-	TF
+	TF,
 } frame_type;
 
-typedef struct
-{
+typedef struct {
 	char *str_data;
 	uint64_t str_len;
 } string_t;
 
 typedef uint64_t integer_t;
 
-typedef struct
-{
+typedef struct {
 	char *name;
 	uint64_t name_len;
 	frame_type var_frame;
 } variable_t;
 
-typedef union
-{
+typedef union {
 	string_t string_val;
 	integer_t int_val;
 	variable_t var_data;
+	opcode_type ins_opcode;
 } token_value_t;
 
-typedef struct
-{
+typedef struct {
 	token_type type;
 	token_value_t value;
-};
+} token_t;
 #endif
