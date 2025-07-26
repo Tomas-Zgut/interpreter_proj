123504f9966fc95694a2bbd9f2be46d0cca309c4
Tomas-Zgut <xzgutt00@stud.fit.vutbr.cz>
2025-07-26 17:12:43 +0200

    added new overloaded functions,fixed type order
    New functions:
     - sb_length
     - sb_concat
     - sb_concat_mut
     - sb_append
    
    Fixed  ordering of types in overload lists so it is consistent
    
    added doc comments,added support for another type
    
    sb free was without documentaion
    Fixed documentation comments for __impl functions
    Added support for non const StringData in overloads.
    Reason: Avoids a lot of casting when calling overloads (not ideal sloution)
    
    added asserts, removed unnecesary code

--- headders/string_buffer.h
+++ headders/string_buffer.h
@@ -53,6 +53,7 @@ typedef struct {
 	_Generic((buff),                                                           \
 		StringBuffer *: __sb_get_substring_impl,                               \
 		const StringBuffer *: __sb_get_substring_impl,                         \
+		StringData *: __sb_get_substring_impl,                           	   \
 		const StringData *: __sb_get_substring_impl,                           \
 		const StringView *: __sb_get_substring_impl)(                          \
 		(buff)->data, (buff)->length, offset, len)
@@ -77,6 +78,7 @@ typedef struct {
 	_Generic((buff),                                                           \
 		StringBuffer *: __sb_get_substring_impl,                               \
 		const StringBuffer *: __sb_get_substring_impl,                         \
+		StringData *: __sb_get_substring_impl,                           	   \
 		const StringData *: __sb_get_substring_impl,                           \
 		const StringView *: __sb_get_substring_impl)(                          \
 		(buff)->data, (buff)->length, offset, (buff)->length)
@@ -98,10 +100,11 @@ typedef struct {
  */
 #define sb_copy(buff, out)                                                     \
 	_Generic((buff),                                                           \
-		const StringBuffer *: __sb_copy_impl,                                  \
 		StringBuffer *: __sb_copy_impl,                                        \
+		const StringBuffer *: __sb_copy_impl,                                  \
+		StringData *: __sb_copy_impl,                                    	   \
 		const StringData *: __sb_copy_impl,                                    \
-		const StringView *: __sb_copy_impl)((buff)->data, (buff)->length, out)
+		const StringView *: __sb_copy_impl)((buff)->data, (buff)->length, (out))
 
 /**
  * @brief Overloaded function for creating a  copy of a string
@@ -121,26 +124,183 @@ typedef struct {
  */
 #define sb_copy_mut(buff, out)                                                 \
 	_Generic((buff),                                                           \
-		const StringBuffer *: __sb_copy_mut_impl,                              \
 		StringBuffer *: __sb_copy_mut_impl,                                    \
+		const StringBuffer *: __sb_copy_mut_impl,                              \
+		StringData *: __sb_copy_mut_impl,                                	   \
 		const StringData *: __sb_copy_mut_impl,                                \
 		const StringView *: __sb_copy_mut_impl)((buff)->data, (buff)->length,  \
-												out)
+												(out))
 
+/**
+ * @brief Overloaded function to free a string type
+ * 
+ * @par Function frees and deinitializes a string type.
+ * Supported buffer types:
+ * - StringBuffer  -> mutable string type @see StringBuffer
+ * - StringData -> owning immutable string type @see StringData
+ * 
+ * @param buff: string to free (type dependent)
+ * 
+ * @see__sb_free
+ * @see _sd_free
+ */
 #define sb_free(buff)                                                          \
-	_Generic((buff), StringBuffer *: __sb_free, StringData *: __sd_free)(buff)
+	_Generic((buff), StringBuffer *: __sb_free, StringData *: __sd_free)((buff))
+
+
+/**
+ * @brief private macro for accesing fields of a string type
+ * 
+ * @par Macro used in the implenetation of a few functions that
+ * reuire generic acces to a specific field
+ * 
+ * @see sb_length
+ * @see sb_concat
+ * @see sb_concat_mut
+ */
+#define __FIELD_ACCES(buff,field)				\
+	_Generic((buff),							\
+		StringBuffer *: (buff)->field, 			\
+		const StringBuffer *: (buff)->field,	\
+		StringData *: (buff)->field, 			\
+		const StringData *: (buff)->field, 		\
+		const StringView *:(buff)->field 		\
+)
+
+/**
+ * @brief Overloaded function that returns the length of a srting type
+ * 
+ * @par function returns the length of a string type
+ * Supported buffer types:
+ * - StringBuffer  -> mutable string type @see StringBuffer
+ * - StringVeiw -> non owning immutable view into a buffer @see StringView
+ * - StringData -> owning immutable string type @see StringData
+ * 
+ * @param buff: string type (type dependent)
+ * 
+ * @returns length of the buffer as a size_t
+ */
+#define sb_length(buff) __FIELD_ACCES(buff,length)
+
+/**
+ * @brief Overloaded function that indexes into a string type
+ * 
+ * @par Function indexes into a string type and returns the char at
+ * the given index.
+ * Supported buffer types:
+ * - StringBuffer  -> mutable string type @see StringBuffer
+ * - StringVeiw -> non owning immutable view into a buffer @see StringView
+ * - StringData -> owning immutable string type @see StringData
+ * 
+ * @param buff: target buffer (type dependent)
+ * @param index: inex of the buffer
+ * 
+ * @returns char at the specified indesx of a raw string
+ * 
+ * @warning No bounds check, use carefuly!
+ * @see __sb_get_char_at_impl
+ */
+#define sb_get_char_at(buff, index) 					\
+	_Generic((buff), 									\
+		StringBuffer *: __sb_get_char_at_impl, 			\
+		const StringBuffer *: __sb_get_char_at_impl,	\
+		StringData *: __sb_get_char_at_impl, 			\
+		const StringData *: __sb_get_char_at_impl, 		\
+		const StringView *:__sb_get_char_at_impl 		\
+)((buff)->data, index)
+
+
+/**
+ * @brief Overloaded function that appends a string to mutable srting
+ * 
+ * @par Function appends the value of a string type to a mutable string
+ * Supported buffer types:
+ * - StringBuffer  -> mutable string type @see StringBuffer
+ * - StringVeiw -> non owning immutable view into a buffer @see StringView
+ * - StringData -> owning immutable string type @see StringData
+ * 
+ * @param[out] out: mutable string output
+ * @param[in] buff: string to append (type dependent)
+ * 
+ * @returns true if the append was succesful, false otherwise
+ * 
+ * @see __sb_append_string_impl
+ */
+#define sb_append_string(out,buff) 						\
+	_Generic((buff), 									\
+		StringBuffer *: __sb_append_string_impl, 		\
+		const StringBuffer *: __sb_append_string_impl,	\
+		StringData *:__sb_append_string_impl,			\
+		const StringData *:__sb_append_string_impl,		\
+		const StringView *:__sb_append_string_impl 		\
+)((out),(buff)->data, (buff)->length)	
+
+/**
+ * @brief Ovlerloaded function concatenates 2 strings and puts them
+ * into an immutable buffer destination.
+ *
+ * @par Funcion concatenates data genericaly between any 2 string types and
+ * saves the result into an immutable string @see StringData.
+ * Supported buffer types:
+ * - StringBuffer  -> mutable string type @see StringBuffer
+ * - StringVeiw -> non owning immutable view into a buffer @see StringView
+ * - StringData -> owning immutable string type @see StringData
+ * 
+ * @param out[out]: immutable srting @see StringData
+ * @param buff1[in]: string to concat (type dependent)
+ * @param buff2[in]: string to concat (type dependent)
+ * 
+ * @returns true if concatenations was succesful, false otherwise.
+ * 
+ * @see sb_concat_impl
+ */
+#define sb_concat(out,buff1, buff2) 	\
+	__sb_concat_impl(					\
+		(out),							\
+		__FIELD_ACCES(buff1,data), 		\
+		__FIELD_ACCES(buff1,length),	\
+		__FIELD_ACCES(buff2,data),		\
+		__FIELD_ACCES(buff2,length))
+
+
+/**
+ * @brief Overloaded function concatenates 2 strings and puts them
+ * into a mutable buffer destination.
+ * 
+ * @par Funcion concatenates data genericaly between any 2 string types and
+ * saves the reuslt into a mutable string @see StringBuffer.
+ * Supported buffer types:
+ * - StringBuffer  -> mutable string type @see StringBuffer
+ * - StringVeiw -> non owning immutable view into a buffer @see StringView
+ * - StringData -> owning immutable string type @see StringData
+ * @param out[out]: mutable srting @see StringBuffer
+ * @param buff1[in]: string to concat (type dependent)
+ * @param buff2[in]: string to concat (type dependent)
+ * 
+ * @returns true if concatenations was succesful, false otherwise.
+ * 
+ * @see __sb_concat_mut_impl
+ */
+#define sb_concat_mut(out,buff1, buff2) 	\
+	__sb_concat_mut_impl(					\
+		(out),								\
+		__FIELD_ACCES(buff1,data), 			\
+		__FIELD_ACCES(buff1,length),		\
+		__FIELD_ACCES(buff2,data),			\
+		__FIELD_ACCES(buff2,length))
+
+
 
 bool sb_init(StringBuffer *buff, size_t initial_capacity);
 bool sb_append_char(StringBuffer *buf, char c);
 void sb_reset(StringBuffer *buf);
 /**
- * @brief Funcion implements creating substrings from a string type
+ * @brief Funcion implements creating substrings from raw string
  *
- * @param buff: pointer to character data
- * @param buff_len: lenght of the character data
- * @param offest: offset into the buffer data where the substring starts
- * @param len: lenght of the substring, if 0 the lenght is till the end of the
- * buffer
+ * @param buff: raw string
+ * @param buff_len: lenght of the raw string
+ * @param offest: offset into the raw string where the substring starts
+ * @param len: lenght of the substring
  *
  * @note This function should not be used @see sb_get_substring
  *
@@ -149,7 +309,7 @@ void sb_reset(StringBuffer *buf);
 StringView __sb_get_substring_impl(const char *buff, size_t buff_len,
 								   size_t offset, size_t len);
 /**
- * @brief Funcion implements freeing a string type
+ * @brief Funcion frees a string type
  *
  * @param buff: buffer to free
  *
@@ -157,7 +317,7 @@ StringView __sb_get_substring_impl(const char *buff, size_t buff_len,
  */
 void __sb_free(StringBuffer *buf);
 /**
- * @brief Funcion implements freeing a string type
+ * @brief Funcion frees a string type
  *
  * @param buff: buffer to free
  *
@@ -166,10 +326,10 @@ void __sb_free(StringBuffer *buf);
 void __sd_free(StringData *buff);
 
 /**
- * @brief This function implements creating an immutable copy of a string
+ * @brief Function implements creating an immutable copy of a raw string
  *
- * @param buff[in]: pointer to character data
- * @param buff_len[in]: length of the character data
+ * @param buff[in]: raw string
+ * @param buff_len[in]: length of raw string
  * @param out[out]: destination for the copy
  *
  * @note This function should not be used @see sb_copy
@@ -179,10 +339,10 @@ void __sd_free(StringData *buff);
 bool __sb_copy_impl(const char *buff, size_t buff_len, StringData *out);
 
 /**
- * @brief This function imlpements creating a mutable copy of a string
+ * @brief This function imlpements creating a mutable copy of a raw string
  *
- * @param buff[in]: pointer to character data
- * @param buff_len[in]: length of the character data
+ * @param buff[in]: raw string
+ * @param buff_len[in]: length of raw string
  * @param out[out]: destination for the copy
  *
  * @note This funstion should not be used @see sb_copy_mut
@@ -190,4 +350,64 @@ bool __sb_copy_impl(const char *buff, size_t buff_len, StringData *out);
  * @returns true if copying was succesfull, false otherwise
  */
 bool __sb_copy_mut_impl(const char *buff, size_t buff_len, StringBuffer *out);
+
+/**
+ * @brief Function that indexes into a raw string
+ * 
+ * @param buff: raw string
+ * @param index: inex of the raw string
+ * 
+ * @warning No bounds check, use carefuly!
+ * 
+ * @note This function should not be used @see sb_get_char_at_impl
+ * 
+ * @returns char at the specified index of a given buffer
+ */
+static inline char __sb_get_char_at_impl(const char *buff, size_t index) {
+	return buff[index];
+}
+
+/**
+ * @brief Function appends a raw string to a mutable string
+ * 
+ * @param out[out]: mutable srting @see StringBuffer
+ * @param buff[in]: raw string  to append
+ * @param buff_len[in]: length of raw string 
+ * 
+ * @note Function should not be used @see sb_append_string
+ * 
+ * @returns true if append was succesful, false otherwise.
+ */
+bool __sb_append_string_impl(StringBuffer *out, const char *buff, size_t buff_len);
+
+/**
+ * @brief Function concatenates 2 string into an immutable string
+ * 
+ * @param out[out]: immutable srting @see StringData
+ * @param buff1[in]: raw string to concatenate
+ * @param buff_len1[in]: length of 1st raw string
+ * @param buff2[in]: raw string to concatenate
+ * @param buff_len2[in]: lenth of 2nd raw string
+ * 
+ * @note Function should not be used @see sb_concat
+ * 
+ * @returns true if concatenation was succesful, false otherwise.
+ */
+bool __sb_concat_impl(StringData *out, const char *buff1, size_t buff_len1, const char *buff2, size_t buff_len2);
+
+
+ /**
+ * @brief Function concatenates 2 strings into a mutable string
+ * 
+ * @param out[out]: mutable srting @see StringBuffer
+ * @param buff1[in]: raw string to concatenate
+ * @param buff_len1[in]: length of 1st raw string
+ * @param buff2[in]: raw string to concatenate
+ * @param buff_len2[in]: lenth of 2nd raw string
+ * 
+ * @note Function should not be used @see sb_concat_mut
+ * 
+ * @returns true if concatenation was succesful, false otherwise.
+ */
+bool __sb_concat_mut_impl(StringBuffer *out, const char *buff1, size_t buff_len1, const char *buff2, size_t buff_len2);
 #endif
--- src/DynBuf.c
+++ src/DynBuf.c
@@ -26,9 +26,7 @@ bool sb_init(StringBuffer *buf, size_t initial_capacity) {
 bool sb_append_char(StringBuffer *buf, char c) {
 	if (buf->length + 1 >= buf->capacity) {
 		size_t new_capacity = buf->capacity * 2;
-		if (new_capacity == 0) {
-			new_capacity = SB_DEFAULT_INITIAL_CAPACITY;
-		}
+
 		char *new_data = (char *)realloc(buf->data, new_capacity);
 		if (new_data == NULL) {
 			perror("Error: Memory reallocation failure");
@@ -46,6 +44,9 @@ bool sb_append_char(StringBuffer *buf, char c) {
 void sb_reset(StringBuffer *buf) { buf->length = 0; }
 
 bool __sb_copy_mut_impl(const char *buf, size_t buff_len, StringBuffer *out) {
+	assert(out != NULL);
+	assert(buf != NULL);
+
 	if (!sb_init(out, buff_len)) {
 		return false;
 	}
@@ -60,10 +61,6 @@ StringView __sb_get_substring_impl(const char *buff, size_t buff_len,
 	assert(offset <= buff_len);
 	assert(len <= buff_len - offset);
 	assert(buff != NULL);
-
-	if (len == 0) {
-		len = buff_len;
-	}
 
 	return (StringView){buff + offset, len};
 }
@@ -86,13 +83,82 @@ bool __sb_copy_impl(const char *buff, size_t buff_len, StringData *out) {
 	return true;
 }
 
+
+bool __sb_append_string_impl(StringBuffer *out, const char *buff, size_t buff_len) {
+	assert(out != NULL);
+	assert(buff != NULL);
+
+	if (SIZE_MAX - buff_len > out->length) {
+		return false;
+	}
+
+	if (out->capacity < out->length + buff_len) {
+		char * new_data = realloc(out->data,out->capacity*2);
+		if (new_data == NULL) {
+			return false;
+		}
+		out->capacity *= 2;
+		out->data = new_data;
+	}
+
+	memcpy(out->data + out->length + 1, buff, buff_len);
+	out->length += buff_len;
+	return true;
+}
+
+bool __sb_concat_mut_impl(StringBuffer *out, const char *buff1, size_t buff_len1, const char *buff2, size_t buff_len2) {
+	assert(out != NULL);
+	assert(buff1 != NULL);
+	assert(buff2 != NULL);
+
+	if (SIZE_MAX - buff_len1 > buff_len2) {
+		out->data = NULL; //safe to free in case of overflow
+		return false;
+	}
+
+	if (!sb_init(out,buff_len1 + buff_len2)) {
+		return false;
+	}
+
+	memcpy(out->data,buff1,buff_len1);
+	memcpy(out->data + buff_len1 + 1, buff2, buff_len2);
+	return true;
+}
+
+bool __sb_concat_impl(StringData *out, const char *buff1, size_t buff_len1, const char *buff2, size_t buff_len2) {
+	assert(out != NULL);
+	assert(buff1 != NULL);
+	assert(buff2 != NULL);
+
+	if (SIZE_MAX - buff_len1 > buff_len2) {
+		out->data = NULL; //safe to free in case of overflow
+		return false;
+	}
+
+	char *data = (char *) malloc(buff_len1 + buff_len2);
+
+	if (data == NULL) {
+		return false;
+	}
+
+	out->length = buff_len1 + buff_len2;
+	memcpy(data,buff1,buff_len1);
+	memcpy(data + buff_len1 + 1, buff2, buff_len2);
+	out->data = data;
+	return true;
+
+}
+
+
 void __sb_free(StringBuffer *buf) {
+	assert(buf != NULL);
 	free(buf->data);
 	buf->data = NULL;
 	buf->length = 0;
 	buf->capacity = 0;
 }
 void __sd_free(StringData *buff) {
+	assert(buff != NULL);
 	free((void *)buff->data);
 	buff->data = NULL;
 	buff->length = 0;
